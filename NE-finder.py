d5cf29d5980460b0e69f2392e3352e914adf7f40
Feng Shi <fengtony686@gmail.com>
2022-04-04 12:50:58 -0400

    add NE-finder

---
+++ NE-finder.py
@@ -0,0 +1,46 @@
+# Two agents with binary signals
+from random import sample
+from signal import signal
+from tqdm import trange
+import numpy as np
+
+
+def computeExpectedPayoff(signalDistribution, myStrategy, yourStrategy, eps):
+    term1 = signalDistribution[0] * (myStrategy[0] * yourStrategy[0] + (1 - myStrategy[0]) * (1 - yourStrategy[0])) + signalDistribution[1] * (myStrategy[0] * (1 - yourStrategy[1]) + (1 - myStrategy[0]) * yourStrategy[1]) + \
+        signalDistribution[2] * (myStrategy[1] * (1 - yourStrategy[0]) + (1 - myStrategy[1]) * yourStrategy[0]) + \
+        signalDistribution[3] * (myStrategy[1] * yourStrategy[1] +
+                                 (1 - myStrategy[1]) * (1 - yourStrategy[1]))
+
+    term2 = np.power(signalDistribution[0] + signalDistribution[1], 2) * (np.power(myStrategy[0], 2) + np.power(1 - myStrategy[0], 2)) + np.power(signalDistribution[2] + signalDistribution[3], 2) * (np.power(myStrategy[1], 2) + np.power(
+        1 - myStrategy[1], 2)) + 2 * (signalDistribution[0] + signalDistribution[1]) * (signalDistribution[2] + signalDistribution[3]) * (myStrategy[0] * (1 - myStrategy[1]) + (1 - myStrategy[0]) * myStrategy[1])
+    term2 *= eps
+
+    term3 = (signalDistribution[0] + signalDistribution[1]) * (signalDistribution[0] + signalDistribution[2]) * (myStrategy[0] * yourStrategy[0] + (1 - myStrategy[0]) * (1 - yourStrategy[0])) + (signalDistribution[0] + signalDistribution[1]) * (signalDistribution[1] + signalDistribution[3]) * (myStrategy[0] * (1 - yourStrategy[1]) + (1 - myStrategy[0]) * yourStrategy[1]) + (
+        signalDistribution[2] + signalDistribution[3]) * (signalDistribution[0] + signalDistribution[2]) * ((1 - myStrategy[1]) * yourStrategy[0] + myStrategy[1] * (1 - yourStrategy[0])) + (signalDistribution[2] + signalDistribution[3]) * (signalDistribution[1] + signalDistribution[3]) * ((1 - myStrategy[1]) * (1 - yourStrategy[1]) + myStrategy[1] * yourStrategy[1])
+
+    return term1 - term2 - term3
+
+
+def findNashEquilibrium(signalDistribution, eps=0.1, samples=30):
+    payoffMatrix1 = np.ones(
+        shape=((samples+1)*(samples+1), (samples+1)*(samples+1)))
+    payoffMatrix1 = payoffMatrix1 * (-999)
+    payoffMatrix2 = np.ones(
+        shape=((samples+1)*(samples+1), (samples+1)*(samples+1)))
+    payoffMatrix2 = payoffMatrix2 * (-999)
+    for i in trange((samples+1)*(samples+1)):
+        for j in range((samples+1)*(samples+1)):
+            payoffMatrix1[i][j] = computeExpectedPayoff(signalDistribution=signalDistribution, myStrategy=[
+                                                        int(i/(samples+1))/samples, (i % (samples+1))/samples], yourStrategy=[int(j/(samples+1))/samples, (j % (samples+1))/samples], eps = eps)
+            payoffMatrix2[i][j] = computeExpectedPayoff(signalDistribution=[signalDistribution[0], signalDistribution[2], signalDistribution[1], signalDistribution[3]], myStrategy=[
+                                                        int(j/(samples+1))/samples, (j % (samples+1))/samples], yourStrategy=[int(i/(samples+1))/samples, (i % (samples+1))/samples], eps = eps)
+    nashEquilibriums = []
+    for j in trange((samples+1)*(samples+1)):
+        i = np.argmax(payoffMatrix1[:, j])
+        if j == np.argmax(payoffMatrix2[i]):
+            nashEquilibriums.append((int(i/(samples+1))/samples, (i%(samples+1))/samples, int(j/(samples+1))/samples, (j%(samples+1))/samples))
+
+    return nashEquilibriums
+
+
+print(findNashEquilibrium([0.6, 0.1, 0.1, 0.2]))
--- simulation.py
+++ simulation.py
@@ -14,13 +14,15 @@ class informationElicitationGame:
         self.reports = np.array([[] for _ in range(self.agentNum)])
         self.options = np.array([[] for _ in range(self.agentNum)])
         self.agents = []
-        
+
     def setAgents(self, strategy):
         for _ in range(self.agentNum):
-            self.agents.append(agent(strategy=strategy, optionNum=self.optionNum))
-    
+            self.agents.append(
+                agent(strategy=strategy, optionNum=self.optionNum))
+
     def optionToReport(self, option, signal):
-        report = (option % np.power(self.optionNum, self.optionNum - signal)) / np.power(self.optionNum, self.optionNum - signal - 1)
+        report = (option % np.power(self.optionNum, self.optionNum - signal)
+                  ) / np.power(self.optionNum, self.optionNum - signal - 1)
         # print(option, signal, int(report))
         return int(report)
 
@@ -32,7 +34,7 @@ class informationElicitationGame:
             if seed < rightBound and seed >= leftBound:
                 return signal
             else:
-                leftBound  = rightBound
+                leftBound = rightBound
         return nan
 
     # Here are the mechanism functions.
@@ -44,7 +46,8 @@ class informationElicitationGame:
             if i == currentReport[currentAgentIndex]:
                 payoff += 1
         payoff /= len(currentReport) - 1
-        payoff -= (self.reports[currentAgentIndex][-1] == currentReport[currentAgentIndex]) / (self.reports.shape[1] + 1)
+        payoff -= (self.reports[currentAgentIndex][-1] ==
+                   currentReport[currentAgentIndex]) / (self.reports.shape[1] + 1)
         payoff += 1
         return payoff
 
@@ -56,7 +59,8 @@ class informationElicitationGame:
             if i == currentReport[currentAgentIndex]:
                 payoff += 1
         payoff /= len(currentReport) - 1
-        payoff -= (self.reports[currentAgentIndex][-1] == currentReport[currentAgentIndex]) / (self.reports.shape[1] + 1)
+        payoff -= (self.reports[currentAgentIndex][-1] ==
+                   currentReport[currentAgentIndex]) / (self.reports.shape[1] + 1)
         agreementTerm = 0
         for i in range(self.agentNum):
             if i != currentAgentIndex and self.reports[i][-1] == currentReport[currentAgentIndex]:
@@ -64,7 +68,7 @@ class informationElicitationGame:
         payoff -= (agreementTerm / (self.agentNum - 1))
         payoff += 1
         return payoff
-    
+
     def selfAgreement(self, currentReport, currentAgentIndex):
         if (self.reports.shape[1] == 0):
             return 0
@@ -73,7 +77,8 @@ class informationElicitationGame:
             if i == currentReport[currentAgentIndex]:
                 payoff += 1
         payoff /= len(currentReport) - 1
-        payoff -= int(self.reports[currentAgentIndex][-1]) == currentReport[currentAgentIndex]
+        payoff -= int(self.reports[currentAgentIndex]
+                      [-1]) == currentReport[currentAgentIndex]
         payoff += 1
         # if currentAgentIndex == 1:
         #     print(currentReport, self.reports[currentAgentIndex][-1])
@@ -95,21 +100,26 @@ class informationElicitationGame:
             currentOptions.append(self.agents[i].chooseOption())
         currentReports = []
         for i in range(self.agentNum):
-            currentReports.append(self.optionToReport(currentOptions[i], signals[i]))
+            currentReports.append(self.optionToReport(
+                currentOptions[i], signals[i]))
         # print(currentReports, currentOptions, signals)
         for i in range(self.agentNum):
             allStrategyPayoff = []
             for strategy in range(self.strategyNum):
                 counterfactualReports = currentReports.copy()
-                counterfactualReports[i] = self.optionToReport(strategy, signals[i])
+                counterfactualReports[i] = self.optionToReport(
+                    strategy, signals[i])
                 if self.mechanism == "Self Agreement":
-                    allStrategyPayoff.append(self.selfAgreement(counterfactualReports, i))
+                    allStrategyPayoff.append(
+                        self.selfAgreement(counterfactualReports, i))
                 elif self.mechanism == "Dynamic Self Agreement":
-                    allStrategyPayoff.append(self.dynamicSelfAgreement(counterfactualReports, i))
-            self.agents[i].possiblePayoffs = np.hstack((self.agents[i].possiblePayoffs, np.array([allStrategyPayoff]).T))
+                    allStrategyPayoff.append(
+                        self.dynamicSelfAgreement(counterfactualReports, i))
+            self.agents[i].possiblePayoffs = np.hstack(
+                (self.agents[i].possiblePayoffs, np.array([allStrategyPayoff]).T))
         self.addReports(currentReports, currentOptions)
         # print(currentReports)
-         
+
 
 class agent:
     def __init__(self, strategy, optionNum):
@@ -125,7 +135,8 @@ class agent:
         if self.strategy == "FLP":
             accumulateRewards = np.array([])
             for i in range(self.strategyNum):
-               accumulateRewards = np.append(accumulateRewards, np.sum(self.possiblePayoffs[i]) + np.random.rand() * 10)
+                accumulateRewards = np.append(accumulateRewards, np.sum(
+                    self.possiblePayoffs[i]) + np.random.rand() * 10)
             return np.argmax(accumulateRewards)
         elif self.strategy == "Replicator Dynamics":
             if len(self.reports) == 0:
@@ -134,12 +145,15 @@ class agent:
             else:
                 accumulateRewards = np.array([])
                 for i in range(self.strategyNum):
-                    accumulateRewards = np.append(accumulateRewards, np.sum(self.possiblePayoffs[i]) + np.random.rand() * 10)
+                    accumulateRewards = np.append(accumulateRewards, np.sum(
+                        self.possiblePayoffs[i]) + np.random.rand() * 10)
                 averageRewards = accumulateRewards / len(self.reports)
-                totalAverage = np.sum([self.strategyProbList[i] * averageRewards[i] for i in range(self.strategyNum)])
+                totalAverage = np.sum(
+                    [self.strategyProbList[i] * averageRewards[i] for i in range(self.strategyNum)])
                 newStrategyProbList = []
                 for i in range(self.strategyNum):
-                    newStrategyProbList.append(self.strategyProbList[i] * averageRewards[i] / totalAverage)
+                    newStrategyProbList.append(
+                        self.strategyProbList[i] * averageRewards[i] / totalAverage)
                 self.strategyProbList = newStrategyProbList
             randomSeed = np.random.rand()
             tmpTotal = 0
@@ -152,32 +166,36 @@ class agent:
             upperConfidenceBound = np.array([])
             for i in range(self.strategyNum):
                 if len(self.reports) != 0:
-                    upperConfidenceBound = np.append(upperConfidenceBound, np.sqrt(np.log(len(self.reports)) / self.options.count(i)) if self.options.count(i) != 0 else np.sqrt(np.log(len(self.reports)) * 2))
+                    upperConfidenceBound = np.append(upperConfidenceBound, np.sqrt(np.log(len(
+                        self.reports)) / self.options.count(i)) if self.options.count(i) != 0 else np.sqrt(np.log(len(self.reports)) * 2))
                 else:
-                    upperConfidenceBound = np.append(upperConfidenceBound, np.random.rand())
+                    upperConfidenceBound = np.append(
+                        upperConfidenceBound, np.random.rand())
             for index, i in enumerate(self.options):
                 for j in range(self.strategyNum):
                     if i == j:
-                        upperConfidenceBound[i] += self.possiblePayoffs[i][index] / self.options.count(i)
+                        upperConfidenceBound[i] += self.possiblePayoffs[i][index] / \
+                            self.options.count(i)
             return np.argmax(upperConfidenceBound)
 
 
-
-
 # newGame = informationElicitationGame({(0, 0): 0.21, (0, 1): 0.01, (1, 0): 0.2, (1, 1): 0.58}, "Dynamic Self Agreement")
-newGame = informationElicitationGame({(0, 0): 0.03, (0, 1): 0.01, (1, 0): 0.01, (1, 1): 0.95}, "Self Agreement")
+newGame = informationElicitationGame(
+    {(0, 0): 0.03, (0, 1): 0.01, (1, 0): 0.01, (1, 1): 0.95}, "Self Agreement")
 newGame.setAgents("UCB")
 # newGame.setAgents("Replicator Dynamics")
 for i in range(1200):
     newGame.run()
 
-print(np.sum(newGame.agents[1].possiblePayoffs[0]),np.sum(newGame.agents[1].possiblePayoffs[1]),np.sum(newGame.agents[1].possiblePayoffs[2]),np.sum(newGame.agents[1].possiblePayoffs[3]))
-print(np.sum(newGame.agents[0].possiblePayoffs[0]),np.sum(newGame.agents[0].possiblePayoffs[1]),np.sum(newGame.agents[0].possiblePayoffs[2]),np.sum(newGame.agents[0].possiblePayoffs[3]))
+print(np.sum(newGame.agents[1].possiblePayoffs[0]), np.sum(newGame.agents[1].possiblePayoffs[1]), np.sum(
+    newGame.agents[1].possiblePayoffs[2]), np.sum(newGame.agents[1].possiblePayoffs[3]))
+print(np.sum(newGame.agents[0].possiblePayoffs[0]), np.sum(newGame.agents[0].possiblePayoffs[1]), np.sum(
+    newGame.agents[0].possiblePayoffs[2]), np.sum(newGame.agents[0].possiblePayoffs[3]))
 
-plt.rcParams['figure.figsize'] = (25.0, 6.0) 
-plt.plot(np.arange(1200),newGame.options[0],'s-',color = 'r',label="Agent X")
-plt.plot(np.arange(1200),newGame.options[1],'o-',color = 'g',label="Agent Y")
+plt.rcParams['figure.figsize'] = (25.0, 6.0)
+plt.plot(np.arange(1200), newGame.options[0], 's-', color='r', label="Agent X")
+plt.plot(np.arange(1200), newGame.options[1], 'o-', color='g', label="Agent Y")
 plt.xlabel("Round")
 plt.ylabel("Strategy")
-plt.legend(loc = "best")
+plt.legend(loc="best")
 plt.show()
